3e8020425bb4a3f81c682801f399598480875053
Jhonnatan <jhonnatan.rangel@gmail.com>
2024-04-25 14:49:29 -0500

    add bootstrap connexion

--- templates/header.php
+++ templates/header.php
@@ -5,24 +5,56 @@ $url_base = "/xpert/";
 //$url_base = "http://localhost/xpert/";
 ?>
 
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Panel de bienvenida</title>
-</head>
-<body>
+    <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+    </head>
 
-    <div class="topnav">
-        <!--<a class="active" href="#home">Inicio</a>-->
-        <a href="<?php echo $url_base;?>secciones/cuenta/">Mi cuenta</a>
-        <a href="<?php echo $url_base;?>secciones/usuarios/">Usuarios</a>
-        <a href="<?php echo $url_base;?>secciones/tours/">Tours</a>
-        <a href="<?php echo $url_base;?>secciones/cupones/">Cupones</a>
-        <a href="<?php echo $url_base;?>secciones/staff/">Staff</a>
-        <a href="<?php echo $url_base;?>secciones/transporte/">Transporte</a>
-        <a href="<?php echo $url_base;?>secciones/notificaciones/">Notificaciones</a>
-        <a href="<?php echo $url_base;?>secciones/reportes/">Reportes</a>
-        <a href="../../cerrar.php">Cerrar</a>
-    </div>
+    <body>
+        <header>
+            
+        <nav class="navbar navbar-expand navbar-light bg-light">
+            <ul class="nav navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo $url_base;?>secciones/cuenta/">Mi cuenta</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo $url_base;?>secciones/usuarios/">Usuarios</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo $url_base;?>secciones/tours/">Tours</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo $url_base;?>secciones/cupones/">Cupones</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo $url_base;?>secciones/staff/">Staff</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo $url_base;?>secciones/notificaciones/">Notificaciones</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo $url_base;?>secciones/reportes/">Reportes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../../cerrar.php">Cerrar</a>
+                </li>
+            </ul>
+        </nav>
+        </header>
+
+        <main class="container">
