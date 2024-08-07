27b9f78b76c175c0ad66490cada85c790ea34413
Jhonnatan <jhonnatan.rangel@gmail.com>
2024-08-06 22:02:37 -0500

    change user avatar

--- secciones/cuenta/index.php
+++ secciones/cuenta/index.php
@@ -22,7 +22,7 @@ session_start();
               <div class="card">
                 <div class="card-body">
                   <div class="d-flex flex-column align-items-center text-center">
-                    <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" class="rounded-circle" width="150">
+                    <img src="../../rs/avatar.jpg" alt="vecteezy.com" class="rounded-circle" width="150">
                     <div class="mt-3">
                       <h4><?php echo $_SESSION['usuario_nombre']?></h4>
                       <p class="text-secondary mb-1"><?php echo $_SESSION['usuario_tipo']?></p>
