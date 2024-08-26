20c688c2344f9b7b7ac4327b38e36f1de14e72c7
Jhonnatan <jhonnatan.rangel@gmail.com>
2024-08-25 23:32:21 -0500

    add connection for remote

--- secciones/reportes/cargar_reporte.php
+++ secciones/reportes/cargar_reporte.php
@@ -1,8 +1,14 @@
 <?php
-$servidor = "localhost";
-$baseDatos = "bd_xpert";
-$usuario = "root";
-$contrasena = "";
+//Descomentar para servidor remoto
+$servidor = "127.0.0.1:3306";
+$baseDatos = "u878617270_bd_xpert";
+$usuario = "u878617270_superbad";
+$contrasena = "Romulo_xpert8";
+//Descomentar para servidor local
+//$servidor = "localhost";
+//$baseDatos = "bd_xpert";
+//$usuario = "root";
+//$contrasena = "";
 
 try {
     $conexion = new PDO("mysql:host=$servidor;dbname=$baseDatos", $usuario, $contrasena);
